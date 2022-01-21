21bc3b6da989ce9e29ebd9374e688a9274599392
Stefano Peruzzi <stefano.peruzzi@magentalab.it>
2022-01-21 15:53:29 +0100

    Aggiornamento SseEmitter per prossimi sviluppi

--- src/main/java/com/luv2code/springdemo/controller/SseEmitterController.java
+++ src/main/java/com/luv2code/springdemo/controller/SseEmitterController.java
@@ -17,99 +17,100 @@ import com.luv2code.springdemo.entity.Address;
 
 @Controller
 public class SseEmitterController {
-	
-	// TODO
+
+    // TODO
     // 1. creare un CONTROLLER che torna la pagina test-sse-01
     // 2. nella pagina, si mette il tag <script type=javascript>
     // 3. nello script si chiama il controller sse (quello di questa classe): 
     // const eventSource = new EventSource('http://localhost:8080/.../sse');
-	
-	@RequestMapping("/testSse1")
-	public String showPage1( ) {
-		return "test-sse-01";
-	}
-	
-	@RequestMapping("/testSse2")
-	public String showPage2( ) {
-		return "test-sse-02";
-	}
-	
+    @RequestMapping("/testSse1")
+    public String showPage1() {
+        return "test-sse-01";
+    }
 
-	@GetMapping("/sse1")
-	public SseEmitter eventEmitter() {
-	   SseEmitter emitter = new SseEmitter(); //12000 here is the timeout and it is optional   (long) 12000
-	   
-	   //create a single thread for sending messages asynchronously
-	   ExecutorService executor = Executors.newSingleThreadExecutor();
-	   executor.execute(() -> {
-	       try {
-	           for (int i = 0; i < 10; i++) {
-	        	   Thread.sleep(1000);
-	        	   emitter.send("message" + i);           
-	           }       
-	       } catch(Exception e) {
-	            emitter.completeWithError(e);       
-	       } finally {
-	            emitter.complete();       
-	       }   
-	   });
-	   executor.shutdown();
-	   return emitter;
-	}
-	
-	@GetMapping("/sse2")
-	public SseEmitter eventEmitter2() {
-	   SseEmitter emitter = new SseEmitter(); //12000 here is the timeout and it is optional   (long) 12000
-	   
-	   //create a single thread for sending messages asynchronously
-	   ExecutorService executor = Executors.newSingleThreadExecutor();
-	   
+    @RequestMapping("/testSse2")
+    public String showPage2() {
+        return "test-sse-02";
+    }
 
-	   
-	   executor.execute(() -> {	
-		   try {
-	           for (int i = 0; i < 8; i++) {
-	    		   int j;
-	        	   Thread.sleep(800);
-	        	   j = i+1;
-	        	   SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
-	      	             .id(String.valueOf(i)) // You can give nay string as id
-	      	             .name("customEventName")
-	      	             .data("chiamata " + j)
-	      	             .reconnectTime(10000); //reconnect time in millis
-	        	   emitter.send(sseEventBuilder);          
-	           } 
-			   		
-		   } catch(Exception e) {
-		           	emitter.completeWithError(e);       
-		   } finally {
-	            	emitter.complete();       
-		   }
-	   });
-	   executor.shutdown();
-	   return emitter;
-	}
+    @GetMapping("/sse1")
+    public SseEmitter eventEmitter() {
+        SseEmitter emitter = new SseEmitter(); //12000 here is the timeout and it is optional   (long) 12000
+
+        //create a single thread for sending messages asynchronously
+//        ExecutorService executor = Executors.newSingleThreadExecutor();
+        nonBlockingService.execute(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    Thread.sleep(1000);
+                    emitter.send("message" + i);
+                }
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            } finally {
+                emitter.complete();
+            }
+        });
+        nonBlockingService.shutdown();
+        return emitter;
+    }
+
+    @GetMapping("/sse2")
+    public SseEmitter eventEmitter2() {
+        SseEmitter emitter = new SseEmitter(); //12000 here is the timeout and it is optional   (long) 12000
+
+        //create a single thread for sending messages asynchronously
+//        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        nonBlockingService.execute(() -> {
+            try {
+                for (int i = 0; i < 8; i++) {
+                    int j;
+                    Thread.sleep(800);
+                    j = i + 1;
+                    SseEmitter.SseEventBuilder sseEventBuilder = SseEmitter.event()
+                            .id(String.valueOf(i)) // You can give nay string as id
+                            .name("customEventName")
+                            .data("chiamata " + j)
+                            .reconnectTime(10000); //reconnect time in millis
+                    emitter.send(sseEventBuilder);
+                }
+
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            } finally {
+                emitter.complete();
+            }
+        });
+        nonBlockingService.shutdown();
+        return emitter;
+    }
+
+    public void inviaNuovoEvento(String idOrdine) {
+        // invia evento a tutti gli emitter
+    }
     
-	  
-	@GetMapping("/sse3")
-	public SseEmitter handleSse() {
-			final ExecutorService nonBlockingService = Executors
-			    .newCachedThreadPool();
-			SseEmitter emitter = new SseEmitter();
-			nonBlockingService.execute(() -> {
-			   try {
-		           for (int i = 0; i < 10; i++) {
-		        	   Thread.sleep(1000);
-				       emitter.send("/ORARIO" + " @ " + new Date());
-				       // we could send more events
-				       emitter.complete();    
-		           }    
-			   } catch (Exception ex) {
-			       emitter.completeWithError(ex);
-			   }
-			});
-	return emitter;
-	}
     
+    final ExecutorService nonBlockingService = Executors
+            .newCachedThreadPool();
+
+    @GetMapping("/sse3")
+    public SseEmitter handleSse() {
+
+        SseEmitter emitter = new SseEmitter();
+        nonBlockingService.execute(() -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    Thread.sleep(1000);
+                    emitter.send("/ORARIO" + " @ " + new Date());
+                    // we could send more events
+                    emitter.complete();
+                }
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
+    }
 
 }
