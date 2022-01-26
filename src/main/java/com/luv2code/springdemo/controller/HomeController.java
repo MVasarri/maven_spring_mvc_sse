5b3ab2d74106aa1c1c4d57d241e4358675bb1299
MatVas <vasarri.matteo.dev@gmail.com>
2022-01-26 17:25:36 +0100

    aggiustamenti file di log

--- src/main/java/com/luv2code/springdemo/controller/HomeController.java
+++ src/main/java/com/luv2code/springdemo/controller/HomeController.java
@@ -1,10 +1,21 @@
 package com.luv2code.springdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
+	
+    private static final Logger logger 
+    = LoggerFactory.getLogger(HomeController.class);
+	
+	public static void main(String[] args) {
+        logger.info("Example log from {}", HomeController.class.getSimpleName());
+
+	}
 	
 	@RequestMapping("/")
 	public String showPage( ) {
